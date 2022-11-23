0b72a6c788fca8f358bad408787e4e98f8155a3c
BoryxKajinyx <71166899+BoryxKajinyx@users.noreply.github.com>
2022-11-23 12:12:47 +0100

    Delete game 2 directory

--- game 2/js/movement.js
+++
@@ -1,8 +0,0 @@
-function movement(){
-    player.x += player.velX;
-    player.y += player.velY;
-    for(let i=0;i<balls.length;i++){
-        balls[i].x+=balls[i].velX;
-        balls[i].y+=balls[i].velY;
-    }
-}
